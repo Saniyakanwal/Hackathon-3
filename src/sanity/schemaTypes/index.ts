8803bcbc8b9f26ab3c837782baad1e7a54686491
Saniyakanwal <160118062+Saniyakanwal@users.noreply.github.com>
2025-01-28 16:00:45 +0500

    Delete src directory

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import food from './food'
-import chef from './chef'
-import { type SchemaTypeDefinition } from 'sanity'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [ food, chef],
-}
