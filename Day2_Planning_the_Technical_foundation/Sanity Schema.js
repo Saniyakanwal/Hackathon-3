7bde9e97d846268baeb3e92f6fa27e05990bf11a
Saniyakanwal <160118062+Saniyakanwal@users.noreply.github.com>
2025-01-28 15:23:00 +0500

    day-2

---
+++ Day2_Planning_the_Technical_foundation/Sanity Schema.js
@@ -0,0 +1,16 @@
+export default{
+    name:'product',
+    type:'document',
+    fields:[
+        {
+            name:'name',
+            type:'string',
+            title:'productname'
+        },
+        {
+            name:'price',
+            type:'number',
+            title:'productprice'
+        },
+    ]
+}
